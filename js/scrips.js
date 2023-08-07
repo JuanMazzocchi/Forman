6b7b99b905ca9be69674029b5380e2963d95a682
Juan Mazzocchi <juanmazzocchi@gmail.com>
2023-08-07 15:45:03 -0300

    cambios de opacidad al cambiar de pagina

--- js/scrips.js
+++ js/scrips.js
@@ -7,3 +7,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+let cambioAservicios=document.getElementById('servicios');
+let cambioAsomos=document.getElementById('somos');
+let cambioAforman=document.getElementById('forman');
+let cambioAporque=document.getElementById('por');
+let cambioAcontacto=document.getElementById('contact');
+
+
+cambioAservicios?.addEventListener('click',()=> cambioDePagina('/servicios.html') );
+
+cambioAsomos?.addEventListener('click',()=>cambioDePagina('/somos.html'));
+
+cambioAforman?.addEventListener('click',()=>cambioDePagina('/index.html'));
+ 
+cambioAporque?.addEventListener('click',()=>cambioDePagina('/index.html#porque'))
+
+cambioAcontacto?.addEventListener('click',()=>cambioDePagina('index.html#formContacto'));
+
+function cambioDePagina(id){
+     
+    let elemento =document.getElementById('bodyServicios') 
+    elemento.style.transition='opacity 1s';
+    elemento.style.opacity='0.1';
+    setTimeout(() => {
+        window.location.href = id;
+      }, 900);
+};
